4c2e63c6d2a4fca79a67f675dad1038dd28ae43a
DharmendraSah-arch <109306358+DharmendraSah-arch@users.noreply.github.com>
2023-09-13 06:03:53 -0700

    ResetPassworDto

---
+++ KoLaB/Models/Dto/ResetPasswordDto.cs
@@ -0,0 +1,10 @@
+﻿namespace KoLaB.Models.Dto
+{
+    public record ResetPasswordDto
+    {
+        public string Email { get; set; }
+        public string EmailToken { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
